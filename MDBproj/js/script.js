54a13d7d331a7c4608a9356200981b180f30f727
sVarlog <40181752+sVarlog@users.noreply.github.com>
2020-02-27 21:51:59 +0200

    fix MDB

---
+++ MDBproj/js/script.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+    $(".navbar-nav ").on("click","a", function (event) {
+        event.preventDefault();
+        var id  = $(this).attr('href'),
+        top = $(id).offset().top;
+        $('body,html').animate({scrollTop: top}, 500);
+    });
+});
