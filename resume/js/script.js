2624e6e3a178fa2346a6bb406b42d24e157b1b93
sVarlog <40181752+sVarlog@users.noreply.github.com>
2020-03-11 15:54:49 +0200

    Update resume(add certificate and site)

--- resume/js/script.js
+++ resume/js/script.js
@@ -1,6 +1,11 @@
-$('.education #wp').on('click', function(e){
-	e.preventDefault();
+$('.education #wp').on('click', function(){
 	$('#wpModal').show(500);
+});
+$('.modal .close').on('click', function(){
+	$('.modal').hide(500);
+});
+$('.education #stepikLayout').on('click', function(){
+	$('#stepikModal').show(500);
 });
 $('.modal .close').on('click', function(){
 	$('.modal').hide(500);
