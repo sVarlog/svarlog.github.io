c327c68023d4b7f2545a5aea1c8b5799019a9a6b
sVarlog <40181752+sVarlog@users.noreply.github.com>
2020-03-11 15:29:20 +0200

    Complete layout MoGo

--- MOGO/script/script.js
+++ MOGO/script/script.js
@@ -4,6 +4,7 @@ $(document).ready(function(){
 	  	items: 1,
 	  	dots: true,
 	  	loop: true,
+	  	margin: 120,
 	  	autoplay:true,
     	autoplayTimeout:10000,
 	  	dotsContainer: '#carousel-custom-dots'
@@ -19,6 +20,7 @@ $(document).ready(function(){
 		items: 1,
 		dots: false,
 		nav: true,
+		margin: 20,
 		loop: true,
 		navText: ['','']
 	});
@@ -28,7 +30,15 @@ $(document).ready(function(){
 		items: 6,
 		dots: false,
 		nav: false,
-		loop: true
+		loop: true,
+		responsive:{
+	        0:{
+	            items:3
+	        },
+	        1000:{
+	            items:6
+	        }
+	    }
 	});
 
 	// Init four carousel(section6)
@@ -40,47 +50,75 @@ $(document).ready(function(){
 		navText: ['','']
 	})
 
-	// About block animate cards
-	let card1 = $('.section1 .card1'),
-		card2 = $('.section1 .card2'),
-		card3 = $('.section1 .card3');
-
-	// Card1
-	$(card1).hover(function(){
-		card1.addClass('moveLeftTop');
-		card2.addClass('moveRight');
-		card3.addClass('moveRight');
-	}, function(){
-		card1.removeClass('moveLeftTop');
-		card2.removeClass('moveRight');
-		card3.removeClass('moveRight');
-	});
-	// Card2
-	$(card2).hover(function(){
-		card2.addClass('moveTop');
-		card1.addClass('moveLeft');
-		card3.addClass('moveRight');
-	}, function(){
-		card2.removeClass('moveTop');
-		card1.removeClass('moveLeft');
-		card3.removeClass('moveRight');
-	});
-	// Card3
-	$(card3).hover(function(){
-		card3.addClass('moveRightTop');
-		card2.addClass('moveLeft');
-		card1.addClass('moveLeft');
-	}, function(){
-		card3.removeClass('moveRightTop');
-		card2.removeClass('moveLeft');
-		card1.removeClass('moveLeft');
-	});
-
-	// Tabs(section4)
-	
-
 	// Modal window map
 	$('.section9').click(function(){
 		$('.section9 .modalWindow').toggleClass('active');
 	});
 });
+
+// Tabs section4
+const tabsHeaders = document.querySelectorAll('[data-tab]');
+const tabsImgs = document.querySelectorAll('[data-wrap]');
+tabsHeaders.forEach(tabsHandler);
+function tabsHandler(item){
+	item.addEventListener('click', tabsClick);
+}
+function tabsClick(){
+	// remove class from all tab content
+	tabsHeaders.forEach(function(item){
+		item.classList.remove('active');
+	});
+
+	tabsImgs.forEach(function(item){
+		item.classList.remove('active');
+	});
+
+	// add class to tab headline
+	this.classList.add('active');
+
+	const tabID = this.dataset.tab;
+
+	// hide img
+	document.querySelectorAll('[data-img]').forEach(function(item){
+		item.style = 'opacity: 0';
+	});
+
+	// hide tabs content
+	document.querySelectorAll('[data-content]').forEach(function(item){
+		item.classList.remove('active');
+		item.style = 'transform: rotateX(100%); height: 0px;';
+	});
+
+	// show tabs content
+	document.getElementById(tabID).classList.add('active');
+	document.getElementById(tabID).style = 'transform: rotateX(0);';
+
+	// show img
+	document.querySelector('[data-wrap=' + tabID + ']').classList.add('active');
+	document.querySelector('[data-img=' + tabID + ']').style = 'opacity: 1; display: block;';
+}
+
+// Menu btn
+const navBtn = document.querySelector('.navbarButton');
+const menu = document.querySelector('.menu');
+navBtn.addEventListener('click', showMenu);
+
+function showMenu(){
+	navBtn.classList.toggle('active');
+	menu.classList.toggle('active');
+}
+// scroll
+const anchors = document.querySelectorAll('.scroll-to');
+
+for(let anchor of anchors){
+	anchor.addEventListener('click', function(e){
+		e.preventDefault();
+
+		const blockID = anchor.getAttribute('href');
+
+		document.querySelector(blockID).scrollIntoView({
+			behavior: 'smooth',
+			block: 'start'
+		});
+	});
+}
