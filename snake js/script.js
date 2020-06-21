0b9b45c24bd8b27e22cd5041e8f8347fe1c35872
sVarlog <40181752+sVarlog@users.noreply.github.com>
2020-06-21 12:06:49 +0300

    Update
    Add WASD controll

--- snake js/script.js
+++ snake js/script.js
@@ -134,16 +134,16 @@ createTarget();
 
 // key 
 window.addEventListener('keydown', function(e){
-    if(e.keyCode == 37 && direction != 'right'){
+    if(e.keyCode == 37 || e.keyCode == 65 && direction != 'right'){
         direction = 'left';
     }
-    if(e.keyCode == 38 && direction != 'down'){
+    if(e.keyCode == 38 || e.keyCode == 87 && direction != 'down'){
         direction = 'up';
     }
-    if(e.keyCode == 39 && direction != 'left'){
+    if(e.keyCode == 39 || e.keyCode == 68 && direction != 'left'){
         direction = 'right';
     }
-    if(e.keyCode == 40 && direction != 'up'){
+    if(e.keyCode == 40 || e.keyCode == 83 && direction != 'up'){
         direction = 'down';
     }
 });
