2caf21ab20c3ac2cd23e361a51c479e2ee27b3ed
Mikhail Nosov <giroild@gmail.com>
2020-10-06 00:12:24 +0300

    Add certificate

--- resume/js/script.js
+++ resume/js/script.js
@@ -1,8 +1,8 @@
 $('.education #wp').on('click', function(){
 	$('#wpModal').show(500);
 });
-$('.modal .close').on('click', function(){
-	$('.modal').hide(500);
+$('.education #stepikJS').on('click', function () {
+	$('#stepikJSModal').show(500);
 });
 $('.education #stepikLayout').on('click', function(){
 	$('#stepikModal').show(500);
