9152383e0633a6eebf6a7e82409ed3da54a83937
sVarlog <giroild@gmail.com>
2020-12-19 13:09:16 +0200

    Fix some bugs

--- test_mobile_app/dist/js/moneyPage/moneyPage.js
+++ test_mobile_app/dist/js/moneyPage/moneyPage.js
@@ -131,12 +131,14 @@ const modals = (modalOpen, modalWrapp) => {
         let t = eStart.target,
             startY = eStart.targetTouches[0].pageY;
         if (t.classList.contains('modal') && type === 'click') {
+            modal.style.transition = ".5s";
             modal.classList.remove('active');
             modal.removeEventListener('touchstart', modalHide);
             document.body.classList.remove('modalActive');
         } else {
             swipe = modal.addEventListener('touchmove', (e) => {
                 if ((e.changedTouches[0].pageY) > (startY + 50)) {
+                    modal.style.transition = ".5s";
                     modal.classList.remove('active');
                     modal.removeEventListener('touchstart', modalHide);
                     swipe = null;
@@ -148,6 +150,7 @@ const modals = (modalOpen, modalWrapp) => {
 
     const modalShow = (e) => {
         e.preventDefault();
+        modal.style.transition = "0s";
         document.body.classList.add('modalActive');
         modal.classList.add('active');
         modal.addEventListener('touchstart', modalHide);
