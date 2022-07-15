e1271fa34e8556c0bcca3090907658167d38c4e6
sVarlog <giroild@gmail.com>
2022-07-16 02:45:54 +0300

    Added new TS certificate, updated URLs of works, updated script.js, updated index.html, removed .min styles

--- resume/js/script.js
+++ resume/js/script.js
@@ -1,52 +1,36 @@
-$('.education #wp').on('click', function(){
-	$('#wpModal iframe').attr('src', $('#wpModal iframe').data('src'));
-	$('#wpModal').show(500);
-});
-$('.education #stepikJS').on('click', function () {
-	$('#stepikJSModal iframe').attr('src', $('#stepikJSModal iframe').data('src'));
-	$('#stepikJSModal').show(500);
-});
-$('.education #stepikLayout').on('click', function(){
-	$('#stepikModal iframe').attr('src', $('#stepikModal iframe').data('src'));
-	$('#stepikModal').show(500);
-});
-$('.education #udemyJS').on('click', function () {
-	$('#udemyModal iframe').attr('src', $('#udemyModal iframe').data('src'));
-	$('#udemyModal').show(500);
-});
-$('.education #layoutWayup').on('click', function () {
-	$('#layoutWModal iframe').attr('src', $('#layoutWModal iframe').data('src'));
-	$('#layoutWModal').show(500);
-});
-$('.education #udemyVueJs').on('click', function () {
-	$('#udemyVueModal iframe').attr('src', $('#udemyVueModal iframe').data('src'));
-	$('#udemyVueModal').show(500);
-});
-$('.education #udemyVueJs').on('click', function () {
-	$('#udemyVueModal iframe').attr('src', $('#udemyVueModal iframe').data('src'));
-	$('#udemyVueModal').show(500);
-});
-$('.education #nodeJS').on('click', function () {
-	$('#nodeJSModal iframe').attr('src', $('#nodeJSModal iframe').data('src'));
-	$('#nodeJSModal').show(500);
-});
-$('.education #gitUdemy').on('click', function () {
-	$('#gitUdemyModal iframe').attr('src', $('#gitUdemyModal iframe').data('src'));
-	$('#gitUdemyModal').show(500);
-});
-$('.education #reactNativeUdemy').on('click', function () {
-	$('#reactNativeUdemyModal iframe').attr('src', $('#reactNativeUdemyModal iframe').data('src'));
-	$('#reactNativeUdemyModal').show(500);
-});
-$('.modal .close').on('click', function(){
-	$('.modal').hide(500);
-});
-(() => {
-	let headings = document.querySelectorAll('.works .worksWrap .headingItems'),
-		items = document.querySelectorAll('.works .worksWrap .projects');
+const certificates = [
+    { name: "wp", modal: "wpModal" },
+    { name: "stepikJS", modal: "stepikJSModal" },
+    { name: "stepikLayout", modal: "stepikModal" },
+    { name: "udemyJS", modal: "udemyModal" },
+    { name: "layoutWayup", modal: "layoutWModal" },
+    { name: "udemyVueJs", modal: "udemyVueModal" },
+    { name: "nodeJS", modal: "nodeJSModal" },
+    { name: "gitUdemy", modal: "gitUdemyModal" },
+    { name: "reactNativeUdemy", modal: "reactNativeUdemyModal" },
+    { name: "TSUdemy", modal: "TSUdemyModal" },
+];
 
-	items.forEach((el, i) => {
-		let n = el.querySelectorAll('.workItem');
-		headings[i].append(`( ${n.length} )`);
-	});
-})();
+for (let value of certificates) {
+    $(`.education #${value.name}`).on("click", function () {
+        $(`#${value.modal} iframe`).attr(
+            "src",
+            $(`#${value.modal} iframe`).data("src")
+        );
+        $(`#${value.modal}`).show(500);
+    });
+}
+
+$(".modal .close").on("click", function () {
+    $(".modal").hide(500);
+});
+
+document.addEventListener("DOMContentLoaded", () => {
+    let headings = document.querySelectorAll(".works .worksWrap .headingItems"),
+        items = document.querySelectorAll(".works .worksWrap .projects");
+
+    items.forEach((el, i) => {
+        let n = el.querySelectorAll(".workItem");
+        headings[i].append(`( ${n.length} )`);
+    });
+});
