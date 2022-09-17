7ed16dd694c5f7f0f2979b18c7a77a3aecc0faf4
sVarlog <giroild@gmail.com>
2022-09-17 17:16:52 +0300

    removed folder

--- test_mobile_app/src/makeFriendCard.js
+++
@@ -1,9 +0,0 @@
-import modals from './modules/modal.js';
-import sendForm from './modules/sendForm.js';
-import validation from './modules/friendCardValidation.js';
-
-window.addEventListener('DOMContentLoaded', () => {
-    modals('button.modalOpen', '.modal');
-    sendForm('.modal', '.modalLoad', '.modalConfirm', '.modalConfirm .send');
-    validation('.modal form', '.inputWrapp .input input', '.inputWrapp .input .card img', '.inputWrapp .input', '.inputWrapp .next', 'button.submit');
-});
