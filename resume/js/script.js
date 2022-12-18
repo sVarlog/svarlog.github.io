ebcca084bc6e44506733d5376e29a6ac8e1f8363
sVarlog <giroild@gmail.com>
2022-12-18 18:29:11 +0200

    Added certificate, added a new work, added prettier

--- resume/js/script.js
+++ resume/js/script.js
@@ -1,46 +1,42 @@
 const certificates = [
-    { name: "wp", modal: "wpModal" },
-    { name: "stepikJS", modal: "stepikJSModal" },
-    { name: "stepikLayout", modal: "stepikModal" },
-    { name: "udemyJS", modal: "udemyModal" },
-    { name: "layoutWayup", modal: "layoutWModal" },
-    { name: "udemyVueJs", modal: "udemyVueModal" },
-    { name: "nodeJS", modal: "nodeJSModal" },
-    { name: "gitUdemy", modal: "gitUdemyModal" },
-    { name: "reactNativeUdemy", modal: "reactNativeUdemyModal" },
-    { name: "TSUdemy", modal: "TSUdemyModal" },
-    { name: "GRQL_udemy", modal: "GRQL_udemy_modal" },
-    { name: "EFSET", modal: "EFSET_modal" },
-    { name: "electron_react_udemy", modal: "electron_react_udemy_modal" },
+    { name: 'wp', modal: 'wpModal' },
+    { name: 'stepikJS', modal: 'stepikJSModal' },
+    { name: 'stepikLayout', modal: 'stepikModal' },
+    { name: 'udemyJS', modal: 'udemyModal' },
+    { name: 'layoutWayup', modal: 'layoutWModal' },
+    { name: 'udemyVueJs', modal: 'udemyVueModal' },
+    { name: 'nodeJS', modal: 'nodeJSModal' },
+    { name: 'gitUdemy', modal: 'gitUdemyModal' },
+    { name: 'reactNativeUdemy', modal: 'reactNativeUdemyModal' },
+    { name: 'TSUdemy', modal: 'TSUdemyModal' },
+    { name: 'GRQL_udemy', modal: 'GRQL_udemy_modal' },
+    { name: 'EFSET', modal: 'EFSET_modal' },
+    { name: 'electron_react_udemy', modal: 'electron_react_udemy_modal' },
+    { name: 'tests_udemy', modal: 'tests_udemy_modal' },
 ];
 
 for (let value of certificates) {
-    $(`.education #${value.name}`).on("click", function () {
-        $(`#${value.modal} iframe`).attr(
-            "src",
-            $(`#${value.modal} iframe`).data("src")
-        );
+    $(`.education #${value.name}`).on('click', function () {
+        $(`#${value.modal} iframe`).attr('src', $(`#${value.modal} iframe`).data('src'));
         $(`#${value.modal}`).show(500);
     });
 }
 
-$(".modal .close").on("click", function () {
-    $(".modal").hide(500);
+$('.modal .close').on('click', function () {
+    $('.modal').hide(500);
 });
 
-document.addEventListener("DOMContentLoaded", () => {
-    const workHeadings = document.querySelectorAll(
-        ".works .worksWrap .headingItems"
-    );
-    const workItems = document.querySelectorAll(".works .worksWrap .projects");
+document.addEventListener('DOMContentLoaded', () => {
+    const workHeadings = document.querySelectorAll('.works .worksWrap .headingItems');
+    const workItems = document.querySelectorAll('.works .worksWrap .projects');
 
     workItems.forEach((el, i) => {
-        let nodeCollection = el.querySelectorAll(".workItem");
+        let nodeCollection = el.querySelectorAll('.workItem');
         workHeadings[i].append(`( ${nodeCollection.length} )`);
     });
 
-    const certHeading = document.querySelector(".education .certCount");
-    const certificates = document.querySelectorAll(".education .educationItem");
+    const certHeading = document.querySelector('.education .certCount');
+    const certificates = document.querySelectorAll('.education .educationItem');
 
     certHeading.innerHTML = `(${certificates.length}) :`;
 });
